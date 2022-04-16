a60e0b57d8c8da12d3d3f436b4d6188d1447218b
asingh1182 <63716489+asingh1182@users.noreply.github.com>
2022-04-16 01:53:34 -0400

    Add files via upload

---
+++ Assignment2/modules/globalvars/output.tf
@@ -0,0 +1,12 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "group7"
+    "App"   = "Web"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "finalproject"
+}
