ea7cefe0760e7398387cbebf2ec36a4dae5041db
swetac2 <103720160+swetac2@users.noreply.github.com>
2022-04-16 01:47:03 -0400

    Update variables.tf

--- Assignment2/Project/nonprod/network/variables.tf
+++ Assignment2/Project/nonprod/network/variables.tf
@@ -5,35 +5,35 @@ variable "default_tags" {
     "App"   = "Web"
   }
   type        = map(any)
-  description = "Default tags to be appliad to all AWS resources"
+  description = "Default tags for all AWS resources"
 }
 
 # Name prefix
 variable "prefix" {
   type        = string
   default     = "project"
-  description = "Name prefix"
+  description = "provides Name prefix"
 }
 
 # Provision public subnets in custom VPC
 variable "public_subnet_cidrs" {
   default     = ["10.1.1.0/24", "10.1.2.0/24"]
   type        = list(string)
-  description = "Public Subnet CIDRs"
+  description = "Public Subnet CIDRs blocks"
 }
 
 # Provision provate subnets in custom VPC
 variable "private_subnet_cidr" {
   default     = ["10.1.3.0/24", "10.1.4.0/24"]
   type        = list(string)
-  description = "Private Subnet CIDRs"
+  description = "Private Subnet CIDRs blocks"
 }
 
 # VPC CIDR range
 variable "vpc_cidr" {
   default     = "10.1.0.0/16"
   type        = string
-  description = "VPC to host static web site"
+  description = "VPC to host static web sites"
 }
 
 # Variable to signal the current environment 
