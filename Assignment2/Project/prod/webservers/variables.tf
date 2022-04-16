a8e6a22c541e6d20b6857761a75be3cf2fd42b5b
VidhiPatel1630 <103758933+VidhiPatel1630@users.noreply.github.com>
2022-04-16 01:43:45 -0400

    Add files via upload

---
+++ Assignment2/Project/prod/webservers/variables.tf
@@ -0,0 +1,36 @@
+# Instance type
+variable "instance_type" {
+  default = {
+    "prod"    = "t2.micro"
+    "nonprod" = "t2.micro"
+  }
+  description = "Type of the instance"
+  type        = map(string)
+}
+
+# Variable to signal the current environment 
+variable "env" {
+  default     = "prod"
+  type        = string
+  description = "Deployment Environment"
+}
+
+variable "ec2_count" {
+  type    = number
+  default = "2"
+}
+
+
+
+variable "my_private_ip" {
+  type        = string
+  default     = "172.31.43.189"
+  description = "Private IP of my Cloud 9 station to be opened in bastion ingress"
+}
+
+# curl http://169.254.169.254/latest/meta-data/public-ipv4
+variable "my_public_ip" {
+  type        = string
+  default     = "54.211.83.92"
+  description = "Public IP of my Cloud 9 station to be opened in bastion ingress"
+}
